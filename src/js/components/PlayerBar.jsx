fec3c6b204623b4e4182969112a5ca7a80e463e5
William Martins <wwwhmartins@gmail.com>
2014-12-07 23:15:52 -0200

    Add PlayerBar events

--- src/js/components/PlayerBar.jsx
+++ src/js/components/PlayerBar.jsx
@@ -1,16 +1,31 @@
 var React = require('react');
 
 var PlayerBar = React.createClass({
+    _onBackwardClick: function() {
+        console.log("[TODO] - Backward");
+    },
+    _onForwardClick: function() {
+        console.log("[TODO] - Forward");
+    },
+    _onPlayPauseClick: function() {
+        console.log("[TODO] - Play/Pause");
+    },
     render: function() {
         return (
             <div className="PlayerBar">
-                <span className="PlayerControl">
+                <span
+                    onClick={this._onBackwardClick}
+                    className="PlayerControl">
                     <span className="fa fa-step-backward"></span>
                 </span>
-                <span className="PlayerControl">
+                <span
+                    onClick={this._onPlayPauseClick}
+                    className="PlayerControl">
                     <span className="fa fa-pause"></span>
                 </span>
-                <span className="PlayerControl">
+                <span
+                    onClick={this._onForwardClick}
+                    className="PlayerControl">
                     <span className="fa fa-step-forward"></span>
                 </span>
             </div>
