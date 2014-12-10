057eabb44b8c28f4b352e0793901c212849e54d5
William Martins <wwwhmartins@gmail.com>
2014-12-10 00:31:46 -0200

    Add watch task

--- tasks/watch.js
+++ tasks/watch.js
@@ -1,3 +1,5 @@
 var gulp = require('gulp');
 
-gulp.watch(['src/js/**/*.jsx', 'src/js/**/*.js'], ['build']);
+gulp.task('watch', function() {
+    gulp.watch(['src/js/**/*.jsx', 'src/js/**/*.js'], ['build']);
+});
